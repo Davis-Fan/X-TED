f679efaa3aaf50b524e3aefcc38cc79fb833b796
Davis Fan <davis@DavisdeMacBook-Pro.local>
2024-01-16 14:09:24 -0500

    update main file

--- X-TED_GPU/main.cu
+++ X-TED_GPU/main.cu
@@ -6,8 +6,9 @@ int main(int argc, char* argv[]) {
     char* file_path_1 = argv[1];
     char* file_path_2 = argv[2];
 
-    for(int i=0; i<20;i++) {
+    for(int i=0; i<5;i++) {
         test(file_path_1, file_path_2);
+        printf("\n");
     }
     return 0;
 }
